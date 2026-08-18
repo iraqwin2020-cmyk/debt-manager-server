44e8fa851c644479e5eaea3b2497959c38ed2c40
iraqwin2020-cmyk <iraqwin2020@gmail.com>
2026-08-18 21:04:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ const MAX_LOGIN_ATTEMPTS = 5;
 const LOCKOUT_MINUTES = 15;
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
